c0eecc604f83f6228d1121dc2086ab3b23170773
Nicosuaarez <nicosuarez170605@gmail.com>
2025-05-22 20:44:41 -0300

    Add files via upload
    Listado de pacientes y ya muestra la fecha

--- pacientes.js
+++ pacientes.js
@@ -2,11 +2,11 @@ const db = require('../db');
 
 const express = require('express');
 const router = express.Router();
-
+//ruta del formulario
 router.get('/nuevo', (req, res) => {
     res.render('pacientes/nuevo');
 });
-
+//ruta para guardar el paciente
 router.post('/nuevo', (req, res) =>{
     const { nombre, apellido, dni, fecha_nacimiento, sexo, telefono, direccion } = req.body;
 
@@ -23,5 +23,18 @@ router.post('/nuevo', (req, res) =>{
         res.send('Paciente registrado correctamente.');
     });
 });
+
+//mostrar los pacientes
+router.get('/', (req, res) => {
+    db.query('SELECT * FROM pacientes', (err, resultados) =>{
+        if (err) {
+            console.error('Error al obtener pacientes:', err);
+            return res.send('Error al obtener pacientes');
+        }
+
+        res.render('pacientes/listado', { pacientes: resultados });
+    });
+});
+
 
 module.exports = router;
