658a7df8d2629a82ef50596bdece3401d4d027b8
Nicosuaarez <nicosuarez170605@gmail.com>
2025-05-26 17:55:00 -0300

    comienzo del css

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const path = require("path");
+app.use(express.static(path.join(__dirname, 'public')));
 const app = express();
 
 app.set('view engine', 'pug');
