d21f7ba822dd4def9ca033bdae5fad5093ef6ee1
Nicosuaarez <nicosuarez170605@gmail.com>
2025-05-26 17:16:37 -0300

    internar pacientes y dar cama funcionando

--- src/routes/internaciones.js
+++ src/routes/internaciones.js
@@ -8,4 +8,23 @@ router.get('/nueva/:id', (req, res) => {
     res.render('internaciones/nueva', { pacienteId });
 });
 
+router.post('/nueva/:id', (req, res) => {
+    const pacienteId = req.params.id;
+    const camaId = req.body.cama_id;
+
+    const query = `
+        INSERT INTO internaciones (paciente_id, cama_id)
+        VALUES (?, ?)
+    `;
+
+    db.query(query, [pacienteId, camaId], (err, result) => {
+        if (err){
+            console.error('Error al registrar la internacion:', err);
+            return res.send('Ocurrio un error al internar un paciente.')            
+        }
+        
+        res.send('El paciente ha sido internado correctamente.');
+    });
+});
+
 module.exports = router;
