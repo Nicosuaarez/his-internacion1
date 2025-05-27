b479089315f16caa48da670e34e6779b7902d37f
Nicosuaarez <nicosuarez170605@gmail.com>
2025-05-27 17:47:29 -0300

    css en algunas secciones

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require ('express');
+const express = require ("express");
 const path = require("path");
-app.use(express.static(path.join(__dirname, 'public')));
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
