8edb6ba13cf8d656bfb3860123afe46e7eeb4cfa
Nicosuaarez <nicosuarez170605@gmail.com>
2025-05-29 17:52:40 -0300

    elimine archivos repetidos

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.render('index', { titulo: 'Bienvenido al sistema HIS de internacion'});
-});
-
-module.exports = router;
