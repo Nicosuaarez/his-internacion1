8d8b8f26788ba65ab7204758c3cdb158039109ea
Nicosuaarez <nicosuarez170605@gmail.com>
2025-06-16 20:53:11 -0300

    carga de base de datos a render

--- src/db.js
+++ src/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host:'localhost',
-    user: 'root',
-    password:'Nicolas17',
-    database: 'his_internacion'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    PORT: process.env.DB_PORT
 });
 
 connection.connect((err) => {
